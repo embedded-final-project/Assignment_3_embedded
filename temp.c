0ae2d1d284a34a4c0a8fa7065957839e2d96f4c7
Nourhan Fayez <nourhan.fayez@tkh.edu.eg>
2021-12-19 01:47:17 +0200

    Update temp.c

--- temp.c
+++ temp.c
@@ -10,13 +10,14 @@
   Lcd_Cmd(_LCD_CURSOR_OFF);
   Lcd_Out(1,1,"Temp:");
   }
-  void Temp_Read(int ADC_pin){
+  float Temp_Read(int ADC_pin){
     float Temp_reading;
     char temp_txt[5];
     Temp_reading = (float)ADC_Read(ADC_pin)*0.48828125;
     FloatToStr_FixLen(Temp_reading,temp_txt,4);
     Lcd_Out(1,6,temp_txt);
     delay_ms(100);
+    return Temp_reading;
  }
  void Display_Temp(float Temp_reading){
       if (Temp_reading>35){
