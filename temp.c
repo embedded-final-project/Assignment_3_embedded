6ea6e74f57b6caeb222fc83ca6a48ddb7ce65a04
hanya12 <78305145+hanya12@users.noreply.github.com>
2021-12-18 23:17:10 +0200

    temp files

---
+++ temp.c
@@ -0,0 +1,30 @@
+  #include "LCD_N.h"
+ void Temp_init(){
+    TRISD = 0;
+    TRISA = 0xFF;
+    TRISE =0;
+    }
+ void LCD_In(){
+  Lcd_Init();
+  Lcd_Cmd(_LCD_CLEAR);
+  Lcd_Cmd(_LCD_CURSOR_OFF);
+  Lcd_Out(1,1,"Temp:");
+  }
+  void Temp_Read(int ADC_pin){
+    float Temp_reading;
+    char temp_txt[5];
+    Temp_reading = (float)ADC_Read(ADC_pin)*0.48828125;
+    FloatToStr_FixLen(Temp_reading,temp_txt,4);
+    Lcd_Out(1,6,temp_txt);
+    delay_ms(100);
+ }
+ void Display_Temp(float Temp_reading){
+      if (Temp_reading>35){
+       Lcd_Out(2,1,"Decreasing"); }
+    else if(Temp_reading<=30){
+       Lcd_Out(2,1,"Increasing");
+ }
+    else {
+      Lcd_Out(2,1,"Reached");
+       }
+}
