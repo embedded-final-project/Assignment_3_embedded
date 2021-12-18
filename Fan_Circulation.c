7d81ae32350cbe3a072fc13fff618ea9dec02a99
Nourhan Fayez <nourhan.fayez@tkh.edu.eg>
2021-12-18 17:22:13 +0200

    Update Fan_Circulation.c

--- Fan_Circulation.c
+++ Fan_Circulation.c
@@ -1,9 +1,21 @@
 void fan_circulation(){
- if (PORTC.F2==1){
+ if (PORTC.F2==1 && PORTC = 0b00000010){
    PORTC = 0b00000001;
    Delay_ms(100);
    }
- else{
+ else if (PORTC.F2==1 && PORTC = 0b00000001){
+   PORTC = 0b00000010;
+   Delay_ms(100);
+ }
+ else if (PORTC.F2==1 && PORTC = 0b00001000){
+   PORTC = 0b00010000;
+   Delay_ms(100);
+   }
+ else if (PORTC.F2==1 && PORTC = 0b00010000){
+   PORTC = 0b00001000;
+   Delay_ms(100);
+   }
+ else if (PORTC.F2==0){
     PORTC = 0b00000010;
     Delay_ms(100);
  }
