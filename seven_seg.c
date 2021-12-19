cc5e77db25ed8ae66a7bc5f3490dd2da9daecca2
neren_elbagoury <78306012+neren2elbagoury@users.noreply.github.com>
2021-12-19 23:16:18 +0200

    Update seven_seg.c

--- seven_seg.c
+++ seven_seg.c
@@ -8,3 +8,14 @@ void SEV_SEG_initialize()
  Digit2 = 0;
 }
 
+
+unsigned char display_digit(unsigned char digit)
+{
+ unsigned char value;
+ unsigned char SEG_MAP[10] =
+ {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
+ value = SEG_MAP[digit];
+ return (value);
+ }
+ 
+ 
