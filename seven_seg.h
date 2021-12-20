3c81aa4f9d198597cbaaf4bf5ac0d4c4771958f9
neren_elbagoury <78306012+neren2elbagoury@users.noreply.github.com>
2021-12-20 22:16:17 +0200

    Update seven_seg.h

--- seven_seg.h
+++ seven_seg.h
@@ -1,5 +1,10 @@
-#define Digit1 PORTA.RA0
-#define Digit2 PORTA.RA1
-void SEV_SEG_initialize();
+//macros [text replacement]
+
+#define Digit1 PORTA.RA0  
+#define Digit2 PORTA.RA1   
+
+//functions prototypes
+
+void SEV_SEG_initialize();  
 unsigned char display_digit(unsigned char digit);
 void SEV_SEG_write_number(unsigned char number);
