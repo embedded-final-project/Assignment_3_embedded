e8852812c7695c75f452c7ee57a499c24632a2a9
Nourhan Fayez <67304801+NoraaFayez@users.noreply.github.com>
2021-12-20 21:50:12 +0200

    Update Team3.c

--- Team3.c
+++ Team3.c
@@ -1,8 +1,12 @@
 #include "fan_control.h"
 #include "temp.h"
 #include "Virtual_Terminal.h"
+#include "seven_seg.h"
+#include "Button.h"
+//#define ENABLE_UART
 
 char temp_txt[5];
+char user;
 
 sbit LCD_RS at LATE2_bit;
 sbit LCD_EN at LATE1_bit;
@@ -15,20 +19,27 @@ sbit LCD_EN_Direction at TRISE1_bit;
 sbit LCD_D4_Direction at TRISD4_bit;
 sbit LCD_D5_Direction at TRISD5_bit;
 sbit LCD_D6_Direction at TRISD6_bit;
-sbit LCD_D7_Direction at TRISD7_bit;
-
-float Temp_reading;
+sbit LCD_D7_Direction at TRISD7_bit; 
 
 void main() {
+ float Temp_reading;
  motor_init();
  Temp_init();
  LCD_In();
+ SEV_SEG_initialize();
+ Delay_ms(1000);
+ 
+ #if defined (ENABLE_UART)
  temp_on_uart();
+ //uart_data(Temp_reading);
+ #endif
  
  while(1){
+    user = button();
+    SEV_SEG_write_number(user);
     Temp_reading = Temp_Read(2);
-    Display_Temp(Temp_reading);
-    fan_operation(Temp_reading);
-    motor_on_uart();
+    Display_Temp(Temp_reading, user);
+    fan_operation(Temp_reading, user);
+    //motor_on_uart();
  }
  }
