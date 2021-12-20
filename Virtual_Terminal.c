d436f369003013cf5a66cff9ef53997a53f0ae05
hanya12 <78305145+hanya12@users.noreply.github.com>
2021-12-20 22:03:38 +0200

    Update Virtual_Terminal.c

--- Virtual_Terminal.c
+++ Virtual_Terminal.c
@@ -1,36 +1,39 @@
 #include "Virtual_Terminal.h"
 
 void temp_on_uart() {
-  UART1_Init(9600);
-  Delay_ms(100);
-  UART1_Write_Text ("Temperature:");
-  UART1_Write(13);
+  TRISC.F6 = 0;       //PORTC and Pin6 is an output pin
+  TRISC.F7 = 1;       //PORTC and Pin7 is an input pin
+  UART1_Init(9600);     //Initializing Uart module at 9600 bps
+  Delay_ms(100);        //wait for Uart module to be more stable for 100 milliseconds 
+  UART1_Write_Text ("Temperature:");      //send data through Uart for microcontroller 
+  UART1_Write(13);    //display text 
   UART1_Write(10);
  }
  
 void uart_data()
 {
- if (UART1_Data_Ready()==1)
+ if (UART1_Data_Ready()==1)     // when the data is received but when ==0 data is not receiving 
  {
  char x;
- x = UART1_Read();
- UART1_Write(x);
+ x = UART1_Read();      //read data received through Uart for microcontroller 
+ UART1_Write(x);      //send data through Uart
  }
  } 
 
  void motor_on_uart()  {
- if (PORTC.F3 = 1)
+ if (PORTC.F3 == 1)   //PORTC and Pin3 is an input
         {
 
-        UART1_Write_Text ("motor rotate to the Right");
+        UART1_Write_Text ("Heater ON");
         UART1_Write(13);
         UART1_Write(10);
         Delay_ms(1000);
         }
- if (PORTC.F3 = 0) {
-        UART1_Write_Text ("motor rotate to the Left");
+ else if (PORTC.F3 == 0) {    //PORTC and Pin3 is an output 
+        UART1_Write_Text ("Cooler ON");
         UART1_Write(13);
         UART1_Write(10);
-        Delay_ms(1000);
+        Delay_ms(1000);   //wait for Uart to be more stable for 1000 millisecond
         }
+}
 }
